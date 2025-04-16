ef5e8b12e90520f7e2fcd6651f5385ebe2b17fe4
Michelyee132 <michaelyyu04@gmail.com>
2025-04-16 01:28:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const path = require('path');
+// server.js
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Root route - serves the navbar page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/typesetting', (req, res) => {
-    res.redirect('https://github.com/Michelyee132/TypeSettings');
+// Route for Climate Crisis
+app.get("/climate-crisis", (req, res) => {
+  res.redirect("https://github.com/Michelyee132/climate-crisis");
 });
 
-app.get('/climate', (req, res) => {
-    res.redirect('https://github.com/Michelyee132/climate-crisis');
+// Route for Typesetting
+app.get("/typesetting", (req, res) => {
+  res.redirect("https://github.com/Michelyee132/TypeSettings");
 });
 
-app.get('/algo', (req, res) => {
-    res.redirect('https://github.com/Michelyee132/four-sorting-algorithms');
+// Route for Four Algorithms
+app.get("/four-algorithms", (req, res) => {
+  res.redirect("https://github.com/Michelyee132/four-sorting-algorithms");
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
