2f8f611b5606ab369ef78d076a5acf927c23c9de
Michelyee132 <michaelyyu04@gmail.com>
2025-04-15 23:03:47 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use(morgan('tiny'))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/typesetting', (req, res) => {
+    res.redirect('https://github.com/Michelyee132/TypeSettings')
+})
+
+app.get('/climate', (req,res) => {
+    res.redirect('https://github.com/Michelyee132/climate-crisis')
+})
+
+app.get('/algo', (req,res) => {
+    res.redirect('https://github.com/Michelyee132/four-sorting-algorithms')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
